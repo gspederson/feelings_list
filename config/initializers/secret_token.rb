c084f0547da5b391ead21903e1867e435b533b96
Greg Pederson <greg@nsightdevelopment.com>
2013-04-29 20:48:49 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeelingsList::Application.config.secret_token = '5118e066366a58d977af6a19284e6d97763bde0c1cf82d0560168042bb0d60db7200a38458c826b00e7f42180aa8ad61f3b6314fd18570e0ab4a6368b65e3f30'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FeelingsList::Application.config.session_store :cookie_store, key: '_feelings_list_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FeelingsList::Application.config.session_store :active_record_store
